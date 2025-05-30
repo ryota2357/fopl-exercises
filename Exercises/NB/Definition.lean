9eec7df8406f4ec95e0a2b148ef7bcfc9164b653
ryota2357 <noreply@ryota2357.com>
2025-05-30 14:48:09 +0900

    add NB type system

--- Exercises/NB/Definition.lean
+++ Exercises/NB/Definition.lean
@@ -1,12 +1,14 @@
+import Mathlib.Tactic.Lemma
+
 namespace NB
 
 /-
-t ::= v
-    | if t₁ then t₂ else t₃
-    | succ(t) | pred(t) | iszero(t)
-v ::= b | n
-b ::= true | false
-n ::= 0 | succ(n)
+  t ::= v
+      | if t₁ then t₂ else t₃
+      | succ(t) | pred(t) | iszero(t)
+  v ::= b | n
+  b ::= true | false
+  n ::= 0 | succ(n)
 -/
 
 inductive NatValue where
@@ -54,3 +56,59 @@ inductive MultiStep : Term → Term → Prop where
 | step : ∀ t₁ t₂ t₃, Step t₁ t₂ → MultiStep t₂ t₃ → MultiStep t₁ t₃
 
 infixl:50 " ⟶* " => MultiStep
+
+inductive TermType where
+| bool : TermType
+| nat : TermType
+
+inductive TypeJudgment : Term → TermType → Prop where
+| bool_value : ∀ b, TypeJudgment (value (Value.bool b)) TermType.bool
+| zero : TypeJudgment zero' TermType.nat
+| succ : ∀ t, TypeJudgment t TermType.nat → TypeJudgment (succ t) TermType.nat
+| pred : ∀ t, TypeJudgment t TermType.nat → TypeJudgment (pred t) TermType.nat
+| iszero : ∀ t, TypeJudgment t TermType.nat → TypeJudgment (iszero t) TermType.bool
+| if_ : ∀ t₁ t₂ t₃, ∀ τ,
+    TypeJudgment t₁ TermType.bool → TypeJudgment t₂ τ → TypeJudgment t₃ τ →
+    TypeJudgment (if_ t₁ t₂ t₃) τ
+
+infixl:40 " ∷ " => TypeJudgment
+
+/-
+  このNBと型付け規則の問題: 次の補題が証明できない
+
+    lemma nat_value_has_nat_type : ∀ n, (value (Value.nat n)) ∷ TermType.nat := by
+      intro n
+      induction n with
+      | zero => exact TypeJudgment.zero
+      | succ n' ih => sorry
+
+  sorryの部分は:
+
+    case succ
+    n' : NatValue
+    ih : value (Value.nat n') ∷ TermType.nat
+    ⊢ value (Value.nat n'.succ) ∷ TermType.nat
+
+  これに対して、次の(1)と(2)の方法などが思いつく。(2)を採用する。
+-/
+
+-- 方法(1): 型付け規則に入れてしまう。
+-- inductive TypeJudgment : ... where
+-- ...
+-- | nat_value : ∀ n, TypeJudgment (value (Value.nat n)) TermType.nat
+
+-- 方法(1)での証明
+-- lemma nat_value_has_nat_type : ∀ n, (value (Value.nat n)) ∷ TermType.nat := by
+--   exact fun n ↦ TypeJudgment.nat_value n
+
+-- 方法(2): succ(n) は値としても項としても同じ形をしている。
+axiom succ_term_nat_eq_term_succ_nat : ∀ n, (succ (value (Value.nat n))) = (value (Value.nat (NatValue.succ n)))
+
+-- 方法(2)での証明
+lemma nat_value_has_nat_type : ∀ n, (value (Value.nat n)) ∷ TermType.nat := by
+  intro n
+  induction n with
+  | zero => exact TypeJudgment.zero
+  | succ n' ih =>
+    rw [← succ_term_nat_eq_term_succ_nat n']
+    exact TypeJudgment.succ (value (Value.nat n')) ih
--- Exercises/NB/Properties.lean
+++ Exercises/NB/Properties.lean
@@ -1,3 +1,4 @@
+import Mathlib.Tactic.Use
 import Exercises.NB.Definition
 
 namespace NB
@@ -58,3 +59,144 @@ theorem term_size_step : ∀ t t', t ⟶ t' → termSize t > termSize t' := by
   | eval_pred_succ n =>
     simp only [termSize]
     apply Nat.lt_add_one
+
+/-
+  NB の型システムの健全性 (= 進行 + 保存) の証明
+  - 進行 (progress) : 型がついたら stuck しない
+      t : τ ならば、t は値か、t ⟶ t' となる t' が存在する
+  - 保存 (preservation) : 評価の進行で型が変わらない
+      t : τ かつ t ⟶ t' ならば t' : τ が成り立つ
+-/
+
+theorem type_progress : ∀ t τ, t ∷ τ → (∃ v, t = Term.value v) ∨ (∃ t', t ⟶ t') := by
+  intros t τ h
+  induction h with
+  | bool_value b =>
+    left
+    use Value.bool b
+  | zero =>
+    left
+    use Value.nat NatValue.zero
+  | succ t' ht ih =>
+    cases ih with
+    | inl h =>
+      left
+      obtain ⟨v, hv⟩ := h
+      rw [hv] at ht
+      cases v with
+      | bool b => cases ht  -- 矛盾
+      | nat n =>
+        use Value.nat (NatValue.succ n)
+        rw [hv]
+        exact succ_term_nat_eq_term_succ_nat n
+    | inr h =>
+      right
+      obtain ⟨t'', h'⟩ := h
+      use Term.succ t''
+      exact Step.eval_succ t' t'' h'
+  | pred t' ht ih =>
+    right
+    cases ih with
+    | inl h =>
+      obtain ⟨v, hv⟩ := h
+      rw [hv] at ht
+      cases v with
+      | bool b => cases ht  -- 矛盾
+      | nat n =>
+        rw [hv]
+        cases n with
+        | zero =>
+          use Term.value (Value.nat NatValue.zero)
+          exact Step.eval_pred_zero
+        | succ n' =>
+          use Term.value (Value.nat n')
+          exact Step.eval_pred_succ n'
+    | inr h =>
+      obtain ⟨t'', h'⟩ := h
+      use Term.pred t''
+      exact Step.eval_pred t' t'' h'
+  | iszero t ht ih =>
+    right
+    cases ih with
+    | inl h =>
+      obtain ⟨v, hv⟩ := h
+      rw [hv] at ht
+      cases v with
+      | bool b => cases ht  -- 矛盾
+      | nat n =>
+        rw [hv]
+        cases n with
+        | zero =>
+          use Term.value (Value.bool BoolValue.true)
+          exact Step.eval_iszero_zero
+        | succ n' =>
+          use Term.value (Value.bool BoolValue.false)
+          exact Step.eval_iszero_succ n'
+    | inr h =>
+      obtain ⟨t'', h'⟩ := h
+      use Term.iszero t''
+      exact Step.eval_iszero t t'' h'
+  | if_ t₁ t₂ t₃ τ' ht₁ ht₂ ht₃ ih₁ ih₂ ih₃ =>
+    right
+    cases ih₁ with
+    | inl h =>
+      obtain ⟨v, hv⟩ := h
+      rw [hv] at ht₁
+      cases v with
+      | nat n => cases ht₁  -- 矛盾
+      | bool b =>
+        rw [hv]
+        cases b with
+        | true =>
+          use t₂
+          exact Step.eval_if_true t₂ t₃
+        | false =>
+          use t₃
+          exact Step.eval_if_false t₂ t₃
+    | inr h =>
+      obtain ⟨t₁', h'⟩ := h
+      use Term.if_ t₁' t₂ t₃
+      exact Step.eval_if t₁ t₁' t₂ t₃ h'
+
+theorem type_preservation : ∀ t t' τ, t ∷ τ ∧ t ⟶ t' → t' ∷ τ := by
+  intros t t' τ h
+  obtain ⟨hl, hr⟩ := h
+  induction hl generalizing t' with
+  | bool_value b =>
+    exfalso
+    cases hr  -- 矛盾
+  | zero =>
+    exfalso
+    cases hr  -- 矛盾
+  | succ t₁ ht₁ ih =>
+    cases hr with
+    | eval_succ _ t₁' h₁ =>
+      apply TypeJudgment.succ
+      apply ih
+      exact h₁
+  | pred t₁ ht₁ ih =>
+    cases hr with
+    | eval_pred_zero => exact ht₁
+    | eval_pred_succ n => exact nat_value_has_nat_type n
+    | eval_pred _ t₁' h₁ =>
+      apply TypeJudgment.pred
+      apply ih
+      exact h₁
+  | iszero t₁ ht₁ ih =>
+    cases hr with
+    | eval_iszero_zero => exact TypeJudgment.bool_value BoolValue.true
+    | eval_iszero_succ n => exact TypeJudgment.bool_value BoolValue.false
+    | eval_iszero _ t₁' h₁ =>
+      apply TypeJudgment.iszero
+      apply ih
+      exact h₁
+  | if_ t₁ t₂ t₃ τ' ht₁ ht₂ ht₃ ih₁ ih₂ ih₃ =>
+    cases hr with
+    | eval_if_true => exact ht₂
+    | eval_if_false => exact ht₃
+    | eval_if _ t₁' _ _ ht₁' =>
+      apply TypeJudgment.if_
+      . apply ih₁
+        exact ht₁'
+      . exact ht₂
+      . exact ht₃
